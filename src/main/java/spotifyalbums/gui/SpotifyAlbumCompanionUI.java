3b70a23703981137c23573564c1a66bf262e3ce3
Tyler Newlin <tnewlin25@gmail.com>
2021-03-13 16:32:45 -0500

    Create alert for SpotifyWebApiExceptions

--- src/main/java/spotifyalbums/gui/SpotifyAlbumCompanionUI.java
+++ src/main/java/spotifyalbums/gui/SpotifyAlbumCompanionUI.java
@@ -6,10 +6,14 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import spotifyalbums.model.SpotifyAlbumSearcher;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -41,9 +45,31 @@ public class SpotifyAlbumCompanionUI extends Application {
                 Album album = searcher.searchForAlbum();
                 informationView.show(album);
             } catch (SpotifyWebApiException exception) {
-                System.out.println("Error: " + exception.getMessage());
+                showAlert(exception);
             }
         }));
+    }
+
+    private void showAlert(SpotifyWebApiException exception){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setHeaderText("There was an error while making requests to spotify.");
+        alert.setContentText("See stacktrace for more details.");
+        alert.getDialogPane().setExpandableContent(setupStackTraceContent(exception));
+        alert.show();
+    }
+
+    private TextArea setupStackTraceContent(SpotifyWebApiException exception) {
+        TextArea messageArea = new TextArea(convertStackTraceToString(exception));
+        messageArea.setWrapText(true);
+        messageArea.setWrapText(false);
+        return messageArea;
+    }
+
+    private String convertStackTraceToString(SpotifyWebApiException exception) {
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+        exception.printStackTrace(printWriter);
+        return stringWriter.toString();
     }
 
 }
