3dfb8ab793ea8bc6b716c422e0f2eff75738a9a7
judek <judeking2000@live.com>
2021-03-13 16:06:13 -0500

    Refactor InformationView to use GridPane
    Searching for an album outputs all album information in column-format

--- src/main/java/spotifyalbums/gui/InformationView.java
+++ src/main/java/spotifyalbums/gui/InformationView.java
@@ -2,38 +2,63 @@ package spotifyalbums.gui;
 
 import com.wrapper.spotify.model_objects.specification.Album;
 import com.wrapper.spotify.model_objects.specification.ArtistSimplified;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.VBox;
+import javafx.geometry.Insets;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import spotifyalbums.model.AlbumDurationCalculator;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 
-public class InformationView extends VBox {
+public class InformationView extends GridPane {
 
-    private final TextArea resultArea = new TextArea();
+    private final GridPane resultArea = new GridPane();
 
     public InformationView() {
-        resultArea.setPrefRowCount(31);
         getChildren().add(resultArea);
+        initializeGrid();
     }
 
     public void show(Album album) {
-        resultArea.clear();
-        StringBuilder builder = new StringBuilder();
-        for (String i : getInformationList(album)) {
-            builder.append(i);
-        }
-        String albumInformation = builder.toString();
-        resultArea.setText(albumInformation);
+        resultArea.getChildren().clear();
+        initializeGrid();
+        formatGrid(album);
     }
 
-    public List<String> getInformationList(Album album) {
+    private List<Text> getInformationList(Album album) {
+        AlbumDurationCalculator durationCalculator = new AlbumDurationCalculator();
+        Duration albumDuration = durationCalculator.calculateAlbumDuration(album);
         ArtistSimplified artistSimplified = album.getArtists()[0];
-        String artist = "Artist: " + artistSimplified.getName() + "\n" + "\n";
-        String releaseDate = "Album release date: " + album.getReleaseDate() + "\n" + "\n";
-        String popularity = "Popularity: " + album.getPopularity().toString() + "\n" + "\n";
-        String albumLabel = "Album label: " + album.getLabel() + "\n" + "\n";
-        return Arrays.asList(artist, releaseDate, popularity, albumLabel);
+        Text artist = new Text(artistSimplified.getName());
+        Text releaseDate = new Text(album.getReleaseDate());
+        Text popularity = new Text(album.getPopularity().toString() + " / 100");
+        Text duration = new Text(String.format("%d" + " minutes", albumDuration.toMinutes()));
+        Text albumLabel = new Text(album.getLabel());
+        return Arrays.asList(artist, releaseDate, popularity, duration, albumLabel);
+    }
+
+    public void formatGrid(Album album) {
+        List<Text> informationList = getInformationList(album);
+        for (int i = 0; i < informationList.size(); i++) {
+            resultArea.add(informationList.get(i), 1, i);
+        }
+    }
+
+    public void initializeGrid() {
+        resultArea.setPrefHeight(300);
+        resultArea.setVgap(10);
+        resultArea.setHgap(20);
+        Text artistLbl = new Text("Artist");
+        Text albumReleaseDateLbl = new Text("Album Release Date");
+        Text popularityLbl = new Text("Popularity");
+        Text durationLbl = new Text("Album Duration");
+        Text albumLabelLbl = new Text("Album Label");
+        resultArea.add(artistLbl, 0, 0);
+        resultArea.add(albumReleaseDateLbl, 0, 1);
+        resultArea.add(popularityLbl, 0, 2);
+        resultArea.add(durationLbl, 0, 3);
+        resultArea.add(albumLabelLbl, 0, 4);
     }
 
 }
