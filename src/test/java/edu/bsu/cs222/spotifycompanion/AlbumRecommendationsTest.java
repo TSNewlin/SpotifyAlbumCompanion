f189c90ffbe5ecf80aa9980e795d3b4fe9a40607
Tyler Newlin <tnewlin25@gmail.com>
2021-03-29 16:09:40 -0400

    Add albums and getAlbums to AlbumRecommendations
    The albums member variable was added so that
    AlbumRecommendations objects would only have a .getAlbums
    method because we don't care about the individual tracks
    recommended by Spotify only the tracks' album. We had to
    keep the tracks member variable in AlbumRecommendations
    because I couldn't find a way for the JsonUtil inner
    class to return AlbumSimplified objects rather than null.
    I don't believe we have to worry about the tracks not
    being initialized before albums because the builder has to
    always be called first.

--- src/test/java/edu/bsu/cs222/spotifycompanion/AlbumRecommendationsTest.java
+++ src/test/java/edu/bsu/cs222/spotifycompanion/AlbumRecommendationsTest.java
@@ -4,7 +4,8 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import edu.bsu.cs222.spotifycompanion.model.AlbumRecommendations;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -14,10 +15,13 @@ import java.util.stream.Collectors;
 
 public class AlbumRecommendationsTest {
 
-    @Test
-    public void testGetTracks() {
+    @ParameterizedTest
+    @CsvSource(delimiter = '|', value = {"0 | How Fleeting, How Fragile",
+    "1 | Automata I", "2 | Wanderer", "3 | From the Gallery of Sleep", "4 | Domino",
+    "5 | Immortal", "6 | It Hates You", "7 | The Act", "8 | Sonder", "9 | Evergreen"})
+    public void testAlbumRecommendationNames(int recommendationNumber, String testTitle) {
         AlbumRecommendations testRecommendations = createTestAlbumRecommendations();
-        Assertions.assertEquals(testRecommendations.getTracks().length, 10);
+        Assertions.assertEquals(testRecommendations.getAlbums()[recommendationNumber].getName(), testTitle);
     }
 
     private AlbumRecommendations createTestAlbumRecommendations() {
