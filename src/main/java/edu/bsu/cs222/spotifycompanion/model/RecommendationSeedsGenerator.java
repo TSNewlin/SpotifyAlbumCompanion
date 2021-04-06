27fa4b69d230037695af42cb9449aa9bb81742cc
Tyler Newlin <tnewlin25@gmail.com>
2021-04-06 13:02:31 -0400

    Decompose track seed loops

--- src/main/java/edu/bsu/cs222/spotifycompanion/model/RecommendationSeedsGenerator.java
+++ src/main/java/edu/bsu/cs222/spotifycompanion/model/RecommendationSeedsGenerator.java
@@ -16,30 +16,40 @@ public class RecommendationSeedsGenerator {
     }
 
     public String generateTracksSeed(Album album) {
-        StringBuilder tracksSeed = new StringBuilder();
         if (album.getTracks().getTotal() == 1) {
             return album.getTracks().getItems()[0].getId();
         }
         if (!totalTracksAndArtistsExceedsSeedLimit(album)) {
-            for (int i = 0; i < album.getTracks().getTotal(); i++) {
-                if (i > 0) {
-                    tracksSeed.append(',');
-                }
-                tracksSeed.append(album.getTracks().getItems()[i].getId());
-            }
+            return createSeedWithAllTrackIds(album);
         } else {
-            for (int i = 0; i < 5 - album.getArtists().length; i++) {
-                if (i > 0) {
-                    tracksSeed.append(',');
-                }
-                tracksSeed.append(album.getTracks().getItems()[i].getId());
-            }
+            return createSeedWithMaxPossibleTrackIds(album);
         }
-        return tracksSeed.toString();
     }
 
     private Boolean totalTracksAndArtistsExceedsSeedLimit(Album album) {
         return album.getArtists().length + album.getTracks().getTotal() > 5;
+    }
+
+    private String createSeedWithAllTrackIds(Album album) {
+        StringBuilder tracksSeed = new StringBuilder();
+        for (int i = 0; i < album.getTracks().getTotal(); i++) {
+            if (i > 0) {
+                tracksSeed.append(',');
+            }
+            tracksSeed.append(album.getTracks().getItems()[i].getId());
+        }
+        return tracksSeed.toString();
+    }
+
+    private String createSeedWithMaxPossibleTrackIds(Album album) {
+        StringBuilder tracksSeed = new StringBuilder();
+        for (int i = 0; i < 5 - album.getArtists().length; i++) {
+            if (i > 0) {
+                tracksSeed.append(',');
+            }
+            tracksSeed.append(album.getTracks().getItems()[i].getId());
+        }
+        return tracksSeed.toString();
     }
 
 }
