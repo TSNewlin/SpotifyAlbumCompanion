1ba743fd91435b6bda433f63cec54eca0fe508e3
Jude King <judeking2000@live.com>
2021-04-13 14:05:02 -0400

    Change ClientCredentialsLoader to use Preferences

--- src/test/java/edu/bsu/cs222/spotifycompanion/ClientCredentialsLoaderTest.java
+++ src/test/java/edu/bsu/cs222/spotifycompanion/ClientCredentialsLoaderTest.java
@@ -7,15 +7,37 @@ import edu.bsu.cs222.spotifycompanion.model.ClientCredentialsLoader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.prefs.Preferences;
 
 public class ClientCredentialsLoaderTest {
 
+    private final Preferences testUserPreferences = Preferences.userNodeForPackage(this.getClass());
+    private final ClientCredentialsLoader credentialsLoader = new ClientCredentialsLoader();
+
     @Test
-    public void testLoadCredentialsFrom() throws IOException {
-        ClientCredentialsLoader credentialsLoader = new ClientCredentialsLoader();
-        List<String> result = credentialsLoader.loadCredentialsFrom("fakecredentials.txt");
-        List<String> actual = Arrays.asList("fake_client_id", "fake_client_secret");
-        Assertions.assertEquals(result, actual);
+    public void testLoadCredentialsFrom_UserID() throws IOException {
+        String expectedUserID = getTestUserID();
+        Preferences actualPreferences = credentialsLoader.loadCredentialsFrom("fakecredentials.txt");
+        String actualUserID = actualPreferences.get("userID", "root");
+        Assertions.assertEquals(expectedUserID, actualUserID);
+    }
+
+    @Test
+    public void testLoadCredentialsFrom_UserSecret() throws IOException {
+        String expectedUserSecret = getTestUserSecret();
+        Preferences actualPreferences = credentialsLoader.loadCredentialsFrom("fakecredentials.txt");
+        String actualUserSecret = actualPreferences.get("userSecret", "root");
+        Assertions.assertEquals(expectedUserSecret, actualUserSecret);
+    }
+
+    private String getTestUserID() {
+        testUserPreferences.put("userID", "kG7jAee21k9hNdGVOIxcgruRMso6zKifD");
+        return testUserPreferences.get("userID", "root");
+    }
+
+    private String getTestUserSecret() {
+        testUserPreferences.put("userSecret", "FAHrrlZ8BVGH2ralukF8QwymnP7t4BBJ3");
+        return testUserPreferences.get("userSecret", "root");
     }
 
 }
