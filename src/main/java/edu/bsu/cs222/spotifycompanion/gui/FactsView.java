13c5bdd9f8b82310c1adc3d7b65b7ed0a997d4e2
Tyler Newlin <tnewlin25@gmail.com>
2021-04-12 22:14:48 -0400

    Refactor FactsView to use ImmutableLists

--- src/main/java/edu/bsu/cs222/spotifycompanion/gui/FactsView.java
+++ src/main/java/edu/bsu/cs222/spotifycompanion/gui/FactsView.java
@@ -1,21 +1,21 @@
 package edu.bsu.cs222.spotifycompanion.gui;
 
+import com.google.common.collect.ImmutableList;
 import com.wrapper.spotify.model_objects.specification.Album;
 import com.wrapper.spotify.model_objects.specification.ArtistSimplified;
 import edu.bsu.cs222.spotifycompanion.model.AlbumDurationCalculator;
 import javafx.scene.text.Text;
 
 import java.time.Duration;
-import java.util.Arrays;
 import java.util.List;
 
 public class FactsView extends InformationView {
 
     public FactsView() {
         super();
-        List<String> labelTextList = Arrays.asList("Title", "Artists", "Release Date", "Spotify Popularity",
+        ImmutableList<String> labelTextList = ImmutableList.of("Title", "Artists", "Release Date", "Spotify Popularity",
                 "Duration", "Record Label");
-        for(int i = 0; i < labelTextList.size(); i++) {
+        for (int i = 0; i < labelTextList.size(); i++) {
             add(new Text(labelTextList.get(i)), 0, i);
         }
     }
@@ -26,22 +26,24 @@ public class FactsView extends InformationView {
     }
 
     private void formatGrid(Album album) {
-        List<Text> informationList = getInformationList(album);
+        List<Text> informationList = makeInformationList(album);
         for (int i = 0; i < informationList.size(); i++) {
             add(informationList.get(i), 1, i);
         }
     }
 
-    private List<Text> getInformationList(Album album) {
+    private List<Text> makeInformationList(Album album) {
         AlbumDurationCalculator durationCalculator = new AlbumDurationCalculator();
         Duration albumDuration = durationCalculator.calculateAlbumDuration(album);
         ArtistSimplified artistSimplified = album.getArtists()[0];
-        return Arrays.asList(new Text(album.getName()),
+        return ImmutableList.of(
+                new Text(album.getName()),
                 new Text(artistSimplified.getName()),
                 new Text(album.getReleaseDate()),
                 new Text(album.getPopularity().toString() + " / 100"),
                 new Text(String.format("%d minutes", albumDuration.toMinutes())),
-                new Text(album.getLabel()));
+                new Text(album.getLabel())
+        );
     }
 
 }
