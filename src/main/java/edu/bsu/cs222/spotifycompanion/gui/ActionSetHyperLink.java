be1d08c17d52563ec5d00c4ce1ae58298ff20e39
Tyler Newlin <tnewlin25@gmail.com>
2021-04-19 10:51:16 -0400

    Change ActionSetHyperLink to have builder

--- src/main/java/edu/bsu/cs222/spotifycompanion/gui/ActionSetHyperLink.java
+++ src/main/java/edu/bsu/cs222/spotifycompanion/gui/ActionSetHyperLink.java
@@ -3,16 +3,44 @@ package edu.bsu.cs222.spotifycompanion.gui;
 import javafx.scene.control.Hyperlink;
 
 import java.net.URI;
+import java.util.Objects;
 
 public class ActionSetHyperLink extends Hyperlink {
 
-    private final String externalUrl;
-    private final URI uri;
+    public static Builder withText(String text){
+        return new Builder(text);
+    }
 
-    public ActionSetHyperLink(String text, String externalUrl, String uri) {
-        super(text);
-        this.externalUrl = externalUrl;
-        this.uri = URI.create(uri);
+    public static final class Builder {
+        private final String text;
+        private URI externalWebLinkUri;
+        private URI spotifyUri;
+
+        public Builder(String text){
+            Objects.requireNonNull(text);
+            this.text = text;
+        }
+
+        public Builder andExternalUrl(String externalUrl){
+            Objects.requireNonNull(externalUrl);
+            this.externalWebLinkUri = URI.create(externalUrl);
+            return this;
+        }
+
+        public ActionSetHyperLink andUri(String uri){
+            Objects.requireNonNull(uri);
+            this.spotifyUri = URI.create(uri);
+            return new ActionSetHyperLink(this);
+        }
+    }
+
+    private final URI externalWebLinkUri;
+    private final URI spotifyUri;
+
+    private ActionSetHyperLink(Builder builder) {
+        super(builder.text);
+        this.externalWebLinkUri = builder.externalWebLinkUri;
+        this.spotifyUri = builder.spotifyUri;
     }
 
 
