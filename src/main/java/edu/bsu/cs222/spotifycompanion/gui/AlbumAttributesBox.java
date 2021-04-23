95d164abd6408b715853934316a69aa55117f3ad
Tyler Newlin <tnewlin25@gmail.com>
2021-04-23 11:07:01 -0400

    Change album attributes positioning

--- src/main/java/edu/bsu/cs222/spotifycompanion/gui/AlbumAttributesBox.java
+++ src/main/java/edu/bsu/cs222/spotifycompanion/gui/AlbumAttributesBox.java
@@ -1,9 +1,12 @@
 package edu.bsu.cs222.spotifycompanion.gui;
 
 import com.wrapper.spotify.model_objects.specification.Album;
+import com.wrapper.spotify.model_objects.specification.ArtistSimplified;
 import com.wrapper.spotify.model_objects.specification.Image;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class AlbumAttributesBox extends VBox {
@@ -11,6 +14,7 @@ public class AlbumAttributesBox extends VBox {
     public AlbumAttributesBox() {
         setPadding(new Insets(5, 0, 0, 0));
         setPrefHeight(175);
+        setAlignment(Pos.TOP_CENTER);
     }
 
     public void show(Album album) {
@@ -19,11 +23,20 @@ public class AlbumAttributesBox extends VBox {
         ImageView albumCoverImageView = new ImageView(albumCover.getUrl());
         ActionSetHyperLink albumHyperLink = ActionSetHyperLink.withText(album.getName())
                 .andExternalUrl(album.getExternalUrls().get("spotify")).andUri(album.getUri());
-        ActionSetHyperLink artistHyperLink = ActionSetHyperLink.withText(album.getArtists()[0].getName())
-                .andExternalUrl(album.getArtists()[0].getExternalUrls().get("spotify"))
-                .andUri(album.getArtists()[0].getUri());
-        getChildren().addAll(albumCoverImageView, albumHyperLink, artistHyperLink);
-        System.out.println(artistHyperLink.getPadding());
+        HBox artistHyperLinkBox = createArtistHyperLinkBox(album);
+        getChildren().addAll(albumCoverImageView, albumHyperLink, artistHyperLinkBox);
+    }
+
+    private HBox createArtistHyperLinkBox(Album album) {
+        HBox artistHyperLinkBox = new HBox();
+        for (ArtistSimplified artist: album.getArtists()) {
+            ActionSetHyperLink artistHyperLink = ActionSetHyperLink.withText(artist.getName())
+                    .andExternalUrl(artist.getExternalUrls().get("spotify"))
+                    .andUri(artist.getUri());
+            artistHyperLinkBox.getChildren().add(artistHyperLink);
+        }
+        artistHyperLinkBox.setAlignment(Pos.TOP_CENTER);
+        return artistHyperLinkBox;
     }
 
 }
